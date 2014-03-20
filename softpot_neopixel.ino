c3bad8a4ccec3163d3ef7620e6dfdb7d1d348b91
Lloyd Ramey <lnr0626@gmail.com>
2014-03-20 14:19:32 -0400

    Code

---
+++ softpot_neopixel.ino
@@ -0,0 +1,53 @@
+#include <Adafruit_NeoPixel.h>
+
+#define PIN 6
+#define SOFTPOT_PIN A0
+
+// Parameter 1 = number of pixels in strip
+// Parameter 2 = Arduino pin number (most are valid)
+// Parameter 3 = pixel type flags, add together as needed:
+//   NEO_KHZ800  800 KHz bitstream (most NeoPixel products w/WS2812 LEDs)
+//   NEO_KHZ400  400 KHz (classic 'v1' (not v2) FLORA pixels, WS2811 drivers)
+//   NEO_GRB     Pixels are wired for GRB bitstream (most NeoPixel products)
+//   NEO_RGB     Pixels are wired for RGB bitstream (v1 FLORA pixels, not v2)
+Adafruit_NeoPixel strip = Adafruit_NeoPixel(8, PIN, NEO_GRB + NEO_KHZ800);
+int sensorValue = 0;
+
+// IMPORTANT: To reduce NeoPixel burnout risk, add 1000 uF capacitor across
+// pixel power leads, add 300 - 500 Ohm resistor on first pixel's data input
+// and minimize distance between Arduino and first pixel.  Avoid connecting
+// on a live circuit...if you must, connect GND first.
+
+void setup() {
+  strip.begin();
+  strip.show(); // Initialize all pixels to 'off'
+  
+}
+
+int range = 1024/3;
+int min_val = 1024/3;
+
+void loop() {
+  sensorValue = (analogRead(SOFTPOT_PIN) - min_val) * 3;
+  for (int i = 0; i < strip.numPixels(); i++) {
+//    if(i * 128 < sensorValue) {
+//      strip.setPixelColor(i, grad(i));
+//    } else {
+//      strip.setPixelColor(i, 0, 0, 0);
+//    }
+    strip.setPixelColor(i, Wheel(sensorValue / 4));
+  }
+  strip.show();
+}
+
+uint32_t Wheel(byte WheelPos) {
+  if(WheelPos < 85) {
+   return strip.Color(WheelPos * 3, 255 - WheelPos * 3, 0);
+  } else if(WheelPos < 170) {
+   WheelPos -= 85;
+   return strip.Color(255 - WheelPos * 3, 0, WheelPos * 3);
+  } else {
+   WheelPos -= 170;
+   return strip.Color(0, WheelPos * 3, 255 - WheelPos * 3);
+  }
+}
